2145ca44277bc43e9796a53e4c7d360c17b3f7c1
dbaspider <dbaspider@163.com>
2022-10-25 17:28:41 +0800

    init code

---
+++ src/test/java/sample/data/jpa/P6spySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P6spySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
